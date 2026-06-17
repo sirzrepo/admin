83b6c009fd3a076ee79510f48603e165de005230
mhatons <mhatons@gmail.com>
2026-06-17 16:00:45 +0100

    updated config

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,21 @@
+// export default {
+//   providers: [
+//     {
+//       domain: process.env.CONVEX_CLERK_URL,
+//       applicationID: "convex",
+//     },
+//   ],
+// };
+
+
 export default {
   providers: [
     {
       domain: process.env.CONVEX_CLERK_URL,
+      applicationID: "convex",
+    },
+    {
+      domain: process.env.SITE_URL,
       applicationID: "convex",
     },
   ],
